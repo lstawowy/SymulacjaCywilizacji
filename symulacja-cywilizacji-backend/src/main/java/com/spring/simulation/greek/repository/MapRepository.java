92922b0fd0cd6261d0279f4c03483e289f05d9ff
lstawowy <l.w.stawowy@gmail.com>
2019-01-09 13:49:29 +0100

    Add initial handle for saving maps in database.

---
+++ symulacja-cywilizacji-backend/src/main/java/com/spring/simulation/greek/repository/MapRepository.java
@@ -0,0 +1,13 @@
+package com.spring.simulation.greek.repository;
+
+import com.spring.simulation.greek.map_generator.Map;
+import java.util.List;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface MapRepository extends MongoRepository<Map, String> {
+
+  Map findById(ObjectId id);
+
+  List<Map> findAll();
+}
