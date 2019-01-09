b3e89018574e461be0871e5df05a469add6827d3
lstawowy <l.w.stawowy@gmail.com>
2019-01-09 13:48:15 +0100

    Extract MapReader for reading and writing images.

---
+++ symulacja-cywilizacji-backend/src/main/java/com/spring/simulation/greek/map_generator/MapReader.java
@@ -0,0 +1,51 @@
+package com.spring.simulation.greek.map_generator;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+import javax.imageio.ImageIO;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
+
+@Slf4j
+public class MapReader {
+
+  private static final ClassLoader CLASS_LOADER = MapReader.class.getClassLoader();
+  private static final String RESOURCES_PATH = "src/main/resources/";
+  private static final String WRITE_FILE_FORMAT = "gif";
+  private static final String IOEXCEPTION_ERROR_MESSAGE = "Envountered IOException :";
+
+  static BufferedImage readResource(String path) {
+    BufferedImage resource = null;
+    try {
+      resource = ImageIO
+          .read(Objects.requireNonNull(CLASS_LOADER.getResourceAsStream(path)));
+    } catch (IOException e) {
+      log.error(IOEXCEPTION_ERROR_MESSAGE, e);
+    }
+    return resource;
+  }
+
+  static void saveImageInResources(BufferedImage bufferedImage) {
+    try {
+      File outputfile = new File(RESOURCES_PATH + "newmap.gif");
+      ImageIO.write(bufferedImage, WRITE_FILE_FORMAT, outputfile);
+    } catch (IOException e) {
+      log.error(IOEXCEPTION_ERROR_MESSAGE, e);
+    }
+  }
+
+  public static byte[] readResourceAsByteArray(String path) {
+    byte[] byteArray = null;
+    try {
+      InputStream resource = Objects.requireNonNull(CLASS_LOADER.getResourceAsStream(path));
+      byteArray = IOUtils.toByteArray(Objects.requireNonNull(resource));
+    } catch (IOException e) {
+      log.error(IOEXCEPTION_ERROR_MESSAGE, e);
+    }
+    return byteArray;
+  }
+
+}
